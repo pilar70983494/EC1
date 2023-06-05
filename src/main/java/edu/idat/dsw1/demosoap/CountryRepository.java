f235ce4f2169b7a66d3cc014f0054372efac945b
pilar <LN70983494@idat.edu.pe>
2023-06-05 10:10:38 -0500

    ec1

--- src/main/java/edu/idat/dsw1/demosoap/CountryRepository.java
+++ src/main/java/edu/idat/dsw1/demosoap/CountryRepository.java
@@ -16,28 +16,60 @@ public class CountryRepository {
 	@PostConstruct
 	public void initData() {
 		Country spain = new Country();
-		spain.setName("Spain");
-		spain.setCapital("Madrid");
+		spain.setName("Lunes");
+		spain.setCapital("Monday");
 		spain.setCurrency(Currency.EUR);
 		spain.setPopulation(46704314);
 
 		countries.put(spain.getName(), spain);
 
 		Country poland = new Country();
-		poland.setName("Poland");
-		poland.setCapital("Warsaw");
+		poland.setName("Martes");
+		poland.setCapital("Tuesday");
 		poland.setCurrency(Currency.PLN);
 		poland.setPopulation(38186860);
 
 		countries.put(poland.getName(), poland);
 
 		Country uk = new Country();
-		uk.setName("United Kingdom");
-		uk.setCapital("London");
+		uk.setName("Miercoles");
+		uk.setCapital("Wednesday");
 		uk.setCurrency(Currency.GBP);
 		uk.setPopulation(63705000);
 
 		countries.put(uk.getName(), uk);
+
+		Country jueves = new Country();
+		jueves.setName("Jueves");
+		jueves.setCapital("Thursday");
+		jueves.setCurrency(Currency.GBP);
+		jueves.setPopulation(63705000);
+
+		countries.put(jueves.getName(), jueves);
+
+		Country viernes = new Country();
+		viernes.setName("Viernes");
+		viernes.setCapital("Friday ");
+		viernes.setCurrency(Currency.GBP);
+		viernes.setPopulation(63705000);
+
+		countries.put(viernes.getName(), viernes);
+
+		Country sabado = new Country();
+		sabado.setName("Sabado");
+		sabado.setCapital("Saturday ");
+		sabado.setCurrency(Currency.GBP);
+		sabado.setPopulation(63705000);
+
+		countries.put(sabado.getName(), sabado);
+
+		Country domingo = new Country();
+		domingo.setName("Domingo");
+		domingo.setCapital("Sunday ");
+		domingo.setCurrency(Currency.GBP);
+		domingo.setPopulation(63705000);
+
+		countries.put(domingo.getName(), domingo);
 	}
 
 	public Country findCountry(String name) {
