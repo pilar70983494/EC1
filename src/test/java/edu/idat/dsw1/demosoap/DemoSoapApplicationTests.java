7592b71bd0ef7509ccfa21661fcae92c6289de76
pilar <LN70983494@idat.edu.pe>
2023-06-05 09:38:54 -0500

    commit1

---
+++ src/test/java/edu/idat/dsw1/demosoap/DemoSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.idat.dsw1.demosoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
